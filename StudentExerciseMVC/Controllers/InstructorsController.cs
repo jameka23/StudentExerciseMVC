4496d2cab65f7dea8f244839ae841f45edd75ece
Jameka Echols <jameka.echols@gmail.com>
2019-07-07 20:42:06 -0500

    details and list works on instructors

--- StudentExerciseMVC/Controllers/InstructorsController.cs
+++ StudentExerciseMVC/Controllers/InstructorsController.cs
@@ -1,24 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExerciseMVC.Models;
 
 namespace StudentExerciseMVC.Controllers
 {
     public class InstructorsController : Controller
     {
+        private readonly IConfiguration _config;
+
+        public InstructorsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
         // GET: Instructors
         public ActionResult Index()
         {
-            return View();
+            List<Instructor> instructors = GetAllInstructors();
+            return View(instructors);
         }
 
         // GET: Instructors/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            Instructor instructor = GetInstructorById(id);
+            return View(instructor);
         }
 
         // GET: Instructors/Create
@@ -87,6 +107,132 @@ namespace StudentExerciseMVC.Controllers
             catch
             {
                 return View();
+            }
+        }
+
+        private Instructor GetInstructorById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = $@"SELECT Id,
+                                                    FirstName,
+                                                    LastName,
+                                                    Slack,
+                                                    CohortId,
+                                                    Speciality
+                                            FROM Instructor
+                                            WHERE Id = @id;";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Instructor instructor = null;
+                    while (reader.Read())
+                    {
+                        instructor = new Instructor
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            Slack = reader.GetString(reader.GetOrdinal("Slack")),
+                            CohortId = reader.GetInt32(reader.GetOrdinal("CohortId")),
+                            Specialty = reader.GetString(reader.GetOrdinal("Specialty"))
+                        };
+                    }
+
+                    reader.Close();
+                    return instructor;
+                }
+            }
+        }
+        private List<Instructor> GetAllInstructors()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = $@"SELECT i.Id,
+                                                i.FirstName,
+                                                i.LastName,
+                                                i.Slack,
+                                                i.Specialty,
+                                                i.CohortId AS CoId,
+                                                c.Id AS CohId,
+                                                c.CohortName
+                                        FROM Instructor i
+                                        JOIN Cohort c ON c.Id = i.CohortId;";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // create a collection to hold the list of instructors
+                    List<Instructor> instructors = new List<Instructor>();
+
+                    while (reader.Read())
+                    {
+                        Instructor instructor = new Instructor
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            Slack = reader.GetString(reader.GetOrdinal("Slack")),
+                            Specialty = reader.GetString(reader.GetOrdinal("Specialty")),
+                            CohortId = reader.GetInt32(reader.GetOrdinal("CoId")),
+                            Cohort = new Cohort
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("CohId")),
+                                CohortName = reader.GetString(reader.GetOrdinal("CohortName"))
+                            }
+                        };
+
+                        // add to the list 
+                        instructors.Add(instructor);
+                    }
+                    // close connection and return the list
+                    reader.Close();
+                    return instructors;
+                }
+            }
+        }
+        private List<Cohort> GetAllCohorts()
+        {
+            // step 1 open the connection
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // step 2. create the query
+                    cmd.CommandText = @"SELECT Id,
+                                                CohortName
+                                        FROM Cohort;";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // create a collection to keep the list of cohorts
+                    List<Cohort> cohorts = new List<Cohort>();
+
+                    // run the query and hold the results in an object
+                    while (reader.Read())
+                    {
+                        Cohort cohort = new Cohort
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            CohortName = reader.GetString(reader.GetOrdinal("CohortName"))
+                        };
+
+                        //add to the list of cohorts 
+                        cohorts.Add(cohort);
+                    }
+
+                    //close the connection and return the list of cohorts
+                    reader.Close();
+                    return cohorts;
+
+                }
             }
         }
     }
